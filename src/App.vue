34c40bb997a59b67d957b72251af5e20c13801ef
masayan1126 <masa199311266@gmail.com>
2022-08-07 14:25:08 +0900

    how-to-detect-reactive-value

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,46 @@
 <template>
-  <p>{{ reactiveHuman.name }}</p>
-  <p>{{ reactiveHuman.age }}</p>
+  <button @click="add">TODOを追加する</button>
+  <p>TODOの全件数：{{ notWorkingCount }}</p>
+  <p>TODOの全件数：{{ workingCount1() }}</p>
+  <p>TODOの全件数：{{ workingCount2 }}</p>
+  <ul>
+    <li v-for="(todo, index) in todoList" :key="index.toString()">
+      {{ todo.title }}【{{ todo.isComplete ? "完了" : "未完了" }}】
+    </li>
+  </ul>
 </template>
 
 <script>
-import { reactive, toRef, toRefs } from "vue";
+import { computed, ref } from "vue";
 
 export default {
   setup() {
-    const human = {
-      name: "masayan",
-      age: 28,
+    const todoList = ref([]);
+
+    const add = () => {
+      const todo = {
+        title: "洗濯",
+        isComplete: false,
+      };
+      todoList.value.push(todo);
     };
-    const reactiveHuman = reactive(human);
 
-    console.log(reactiveHuman); // Proxy {name: 'masayan', age: 28}
-    console.log(reactiveHuman.age); // 28
+    const notWorkingCount = todoList.value.length;
 
-    reactiveHuman.age = 25; // OK(.valueは不要)
+    const workingCount1 = () => {
+      return todoList.value.length;
+    };
+    const workingCount2 = computed(() => {
+      return todoList.value.length;
+    });
 
-    console.log(reactiveHuman); // Proxy {name: 'masayan', age: 25}
-
-    // reactiveHuman = {
-    //   name: "hogemaru",
-    //   age: 35,
-    // }; // Uncaught TypeError: Assignment to constant variable.(再代入不可。refなら可)
-
-    // const { name, age } = reactiveHuman; // NG(name, ageはリアクティブではなくなる)
-    // const name = toRef(reactiveHuman, "name"); // 単一(toRef(オブジェクト, 対象のキー))
-    const { name, age } = toRefs(reactiveHuman); // 複数
-
-    return { reactiveHuman, name, age };
+    return {
+      todoList,
+      add,
+      notWorkingCount,
+      workingCount1,
+      workingCount2,
+    };
   },
 };
 </script>
